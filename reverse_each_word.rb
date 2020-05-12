af86cf389d578e72a41f77baf1e828e02a860b7c
Tyler Firth <tyfirth@gmail.com>
2020-05-12 17:11:53 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(string)
+  
+  words = []
+  backwords = []
+  
+  words << string.split(" ") 
+  
+  words.each do |word|
+    backwords << word.reverse 
+  end
+  
+  backwords
+  
+end

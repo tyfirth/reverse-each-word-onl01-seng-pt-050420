e2b3ff55f3e0562c570299706acd73e4b16fa902
Tyler Firth <tyfirth@gmail.com>
2020-05-12 17:34:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,9 +13,9 @@ def reverse_each_word(string)
   
 end
 
-def reverse_each_word(string)
+def reverse_each_word(sentence)
   
-  new_sentence = string.split(" ")
+  new_sentence = sentence.split(" ")
   
   new_sentence.collect do |word|
     word.reverse 

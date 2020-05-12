179aa3204b1e641fa384ff8781f901c3b3cce392
Tyler Firth <tyfirth@gmail.com>
2020-05-12 17:40:34 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,26 @@
+def reverse_each_word(string)
+  
+  words = []
+  backwords = []
+  
+  words = string.split(" ")
+  
+  words.each do |word|
+    backwords << word.reverse 
+  end
+  
+  backwords.join(" ")
+  
+end
+
+def reverse_each_word(sentence)
+  
+  new_array = sentence.split(" ")
+  new_sentence = []
+  
+  new_array.collect do |word|
+   new_sentence << word.reverse 
+   
+    end
+    new_sentence.join(" ")
+end
